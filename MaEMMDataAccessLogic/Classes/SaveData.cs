ea5dd9e607cfb22923d7c07ca30ffd8e60061a8a
Kathrine Hille <Kathrine@kahns-hille.dk>
2019-10-30 13:05:40 +0100

    Savedata updated with more code

--- MaEMMDataAccessLogic/Classes/SaveData.cs
+++ MaEMMDataAccessLogic/Classes/SaveData.cs
@@ -12,9 +12,19 @@ namespace MaEMMDataAccessLogic
 
         public void saveDatastring(string dataArranged)
         {
-            if(!File.Exists)
+            if(File.Exists) //her skal stå noget andet for at se om filen eksisterer på SD-kortet 
             {
+                Stream s = File.Open(path, FileMode.Append); //sti skal tilføjes
+                StreamWriter sw = new StreamWriter(s);
 
+                sw.WriteLine(dataArranged);
+            }
+            else
+            {
+                Stream s = File.Open(path, FileMode.CreateNew); //sti skal tilføjes 
+                StreamWriter sw = new StreamWriter(s);
+
+                sw.WriteLine(dataArranged);
             }
            
         }
