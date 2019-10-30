a10dc0211144c767000829fa2b7e7aa42c75b3cf
Kathrine Hille <Kathrine@kahns-hille.dk>
2019-10-30 10:40:28 +0100

    DTO updating and creating

--- MaeMMBusinessLogic/DTO/XYDTO.cs
+++ MaeMMBusinessLogic/DTO/XYDTO.cs
@@ -8,5 +8,13 @@ namespace MaeMMBusinessLogic
 {
     class XYDTO
     {
+        public XYDTO(double X_, double Y_)
+        {
+            X = X_;
+            Y = Y_; 
+        }
+
+        public double X { get; set; }
+        public double Y { get; set; }
     }
 }
---
+++ MaeMMBusinessLogic/DataPCParameterDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaeMMBusinessLogic
+{
+    class DataPCParameterDTO
+    {
+        public DataPCParameterDTO(double armLength_, string strengthLevel_)
+        {
+            armLength = armLength_;
+            strengthLevel = strengthLevel_; 
+        }
+
+        public double armLength { get; set; }
+
+        public string strengthLevel { get; set; }
+    }
+}
