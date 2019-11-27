421a84a0603c143ea95920140356d3a8a779ac72
MathiasAKAchampen <201509795@post.au.dk>
2019-11-27 12:47:13 +0100

    Added a few lines of code. The start of calculating maximum and explosive

--- MaeMMBusinessLogic/Classes/DataPresenter.cs
+++ MaeMMBusinessLogic/Classes/DataPresenter.cs
@@ -10,7 +10,7 @@ namespace MaeMMBusinessLogic
     {
         private double maxMuscle = 0;
         private double expMuscle = 0;
-        private List<double> testnumbers;
+        private List<double> muscleForce;
         public event EventHandler<SendCoordinateEvent> sendCoordinate;
         private IDataCalculator datacalculator;
 
@@ -18,7 +18,7 @@ namespace MaeMMBusinessLogic
         {
             datacalculator = datacalc;
             datacalculator.sendCoordinate += sendCoordinates;
-            testnumbers = new List<double>();
+            muscleForce = new List<double>();
         }
 
         public void meassure()
@@ -29,15 +29,31 @@ namespace MaeMMBusinessLogic
 
         public void sendCoordinates(object sender, SendCoordinateEvent e)
         {
-            testnumbers.Add(e.y);
+            //if(e.y > maxMuscle) //Måske dette skal ændres til et gennemsnit af flere, så man tager højde for støj
+            //{
+            //    maxMuscle = e.y;
+            //}
+
+            //if(muscleForce.Count<11)
+            //{
+            //    muscleForce.Add(e.y);
+            //}
+            //else
+            //{
+            //    //Remove one from list and add a new one, then calculate slope
+            //    //expMuscle = slope
+            //}
+            ////Ovenstående bør måske overvejes at skulle gøres i en anden selvstændig tråd, snak med Michael
+            
+            muscleForce.Add(e.y); //TESTER brug ovenstående rigtigt
             SendCoordinateEvent SCevent = new SendCoordinateEvent(e.x, e.y);
             sendCoordinate?.Invoke(this, SCevent);
         }
 
         public MaxExpDTOP showResult()
         {
-            maxMuscle = testnumbers.Average();
-            expMuscle = testnumbers.Count();
+            maxMuscle = muscleForce.Average();
+            expMuscle = muscleForce.Count();
 
             MaxExpDTOP MaxDTO = new MaxExpDTOP(maxMuscle, expMuscle);
             return MaxDTO;
